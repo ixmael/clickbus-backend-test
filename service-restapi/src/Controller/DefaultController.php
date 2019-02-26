a5543ad0210eeca301f4042172ca7a170075d8fc
Ismael Ramos <ixmael.io@outlook.com>
2019-02-26 16:48:55 -0600

    Add initial controller

---
+++ service-restapi/src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="default")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/DefaultController.php',
+        ]);
+    }
+}
